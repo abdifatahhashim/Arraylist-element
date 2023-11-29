c60a58a93f7b9b4cd86f0a8e4c9b1e79db4d648b
abdifatahhashim <144829792+abdifatahhashim@users.noreply.github.com>
2023-11-29 17:45:17 +0300

    Add files via upload

---
+++ Element.java
@@ -0,0 +1,18 @@
+package Arraylist;
+import java.util.*;
+//Write a Java program to create a new array list, add some elements (string) and print out the collection
+  class add_Elements {
+
+    public static void main(String[] args)
+    {
+        ArrayList<String> ArrayList_Str = new ArrayList<String>();
+        ArrayList_Str.add("Apple");
+        ArrayList_Str.add("Banana");
+        ArrayList_Str.add("Cherry");
+        ArrayList_Str.add("Pineapple");
+        ArrayList_Str.add("Guava");
+        ArrayList_Str.add("Papaya");
+        ArrayList_Str.add("Plum");
+        System.out.println(ArrayList_Str);
+    }
+}
